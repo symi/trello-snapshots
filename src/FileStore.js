b6a38b563ca662ed111d112392a240c47b0cd1de
Simon Levy <simonjlevy@gmail.com>
2016-02-08 22:06:19 +0000

    FileStore class now has methods: readAll - gets all the data files read - gets a data file at a certain date or between two dates readLastX - gets the latest x number of data files readLatest - get the latest data file

--- src/FileStore.js
+++ src/FileStore.js
@@ -10,8 +10,8 @@ class FileStore {
         this._path = path;
     }
     
-    *readAll() {
-        const data = [];
+    *_getFileNames() {
+        const fileNames = [];
 
         yield mkdirp(this._path);
         
@@ -22,17 +22,92 @@ class FileStore {
             
             if(!stats.isFile()) continue;
             
-            data.push({
-                timeStamp: moment(fileName, DATE_FORMAT).toDate(),
-                data: require(this._path + '\\' + fileName)
-            });
+            fileNames.push(fileName)
+        }
+        
+        return fileNames;
+    }
+    
+    _getData(fileName) {
+        return {
+            timestamp: moment(fileName, DATE_FORMAT).toDate(),
+            data: require(this._path + '\\' + fileName)
+        };
+    }  
+        
+    _orderFileNames(fileNames) {
+        return fileNames
+            .map(fileName => {
+                return { 
+                    timestamp: this._getTimestamp(fileName), 
+                    name: fileName
+                };
+            })
+            .sort((a, b) => a.timestamp > b.timestamp);
+    }
+    
+    *readAll() {
+        const fileNames = yield* this._getFileNames(),
+            data = [];      
+        
+        for (const fileName of fileNames) {
+            data.push(this._getData(fileName));
         }
         
         return data;
     }
     
-    read() {
+    *read(date1, date2) {
+        if (date1 == null) {
+            throw new Error('A date must be provided');
+        }
         
+        if (!moment(date1).isValid()) {
+            throw new Error('An invalid first date was provided');
+        }
+        
+        if (date2 != null && !moment(date2).isValid()) {
+            throw new Error('An invalid second date was provided');
+        }
+        
+        const start = date1,
+            end = date2,
+            data = [];
+        
+        let fileNames = this._orderFileNames(yield* this._getFileNames());  
+          
+        if (end == null) {
+            fileNames = fileNames.filter(fileName => +fileName.timestamp === +start) || [];
+        } else {
+            fileNames = fileNames.filter(fileName => fileName.timestamp >= start && fileName.timestamp <= end) || [];
+        }
+        
+        for (const fileName of fileNames) {
+            data.push(this._getData(fileName.name));
+        }
+            
+        return data;
+    }
+    
+    *readLastX(count) {
+        if (!Number.isInteger(count) || count < 1) {
+            throw new Error('Last count must be a positive integer'); 
+        }
+        const data = [];
+        let fileNames = this._orderFileNames(yield* this._getFileNames());
+        
+        fileNames = fileNames.slice(-count);
+        
+        for (const fileName of fileNames) {
+            data.push(this._getData(fileName.name));
+        }
+            
+        return data;
+    }
+    
+    *readLatest() {
+        const fileNames = this._orderFileNames(yield* this._getFileNames());            
+        return this._getData(fileNames[fileNames.length - 1].name);
     }
     
     *write(data, date) {
